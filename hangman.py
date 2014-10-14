fd5add258bfea7493d6849c39f569aa6cd83ad40
Barry Hanna <barryahanna@gmail.com>
2014-10-14 21:44:26 +0100

    Removed comments from source

--- hangman.py
+++ hangman.py
@@ -38,25 +38,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-""" Concepts:
-previous knowledge:
-data-types
-selection
-arithmetic operations
-relational operations
-boolean operations
-
-not covered:
-global variables
-random number generation
-procedural programming
-
-lessons:
-1) variable declaration and assignment / input&output
-2) arrays
-3) iteration
-4) functions
-5) string manipulation
-6) designing a game (leading to hangman)
-
-"""
